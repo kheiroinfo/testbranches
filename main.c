e958cc27bf450bb53c319337ed1f995fef2d304c
kheiroinfo <133672577+kheiroinfo@users.noreply.github.com>
2023-12-28 14:23:47 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,c,d,f;
+   printf("donner a "); scanf("%d",&a);
+   printf("donner b "); scanf("%d",&b);
+   c=a+b;
+   printf("c=%d\n",c);
+   d=a+b+c;
+   printf("d=%d\n",d);
+   f=a*b;
+   printf("f=%d",f);
+
+    return 0;
+}
